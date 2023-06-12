877b51c44245a7db6937ad03189595fa63bfc709
LJW <king2992@naver.com>
2023-06-12 21:42:40 +0900

    메인 화면 이미지 작업중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,58 +8,12 @@ import {
 import vg22 from "../style/images/vg_22.jpg";
 import vg25 from "../style/images/vg_25.jpg";
 import vg26 from "../style/images/vg_26.jpg";
-import vg4 from "../style/images/vg_4.jpg";
 
-import styles from "../style/Main.css";
 import "../style/Main.css";
 
 const IMAGES = [vg22, vg25, vg26];
 
 export default function Main() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const springApi = useSpringRef();
-
-  const transitions = useTransition(activeIndex, {
-    from: {
-      clipPath: "polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)",
-    },
-    enter: {
-      clipPath: "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)",
-    },
-    leave: {
-      clipPath: "polygon(100% 0%, 100% 100%, 100% 100%, 100% 0%)",
-    },
-    onRest: (_springs, _ctrl, item) => {
-      if (activeIndex === item) {
-        setActiveIndex(activeIndex === IMAGES.length - 1 ? 0 : activeIndex + 1);
-      }
-    },
-    exitBeforeEnter: true,
-    config: {
-      duration: 3000,
-    },
-    delay: 1000,
-    ref: springApi,
-  });
-
-  const springs = useSpring({
-    from: {
-      strokeDashoffset: 120,
-    },
-    to: {
-      strokeDashoffset: 0,
-    },
-    config: {
-      duration: 1000,
-    },
-    loop: true,
-    ref: springApi,
-  });
-
-  useLayoutEffect(() => {
-    springApi.start();
-  }, [activeIndex]);
-
   return (
     <>
       <div className="main_text-bg">
@@ -71,23 +25,16 @@ export default function Main() {
         </div>
       </div>
 
-      <div className="line_y" />
-      <div className="line_w" />
-
-      <div className="container">
+      {/* <div className="container">
         <div className="container__inner">
-          {transitions((springs, item) => (
-            <animated.div className="img__container" style={springs}>
-              <img src={IMAGES[item]} />
-            </animated.div>
-          ))}
+          <img src={vg26} />
         </div>
-      </div>
+      </div> */}
 
-      {/* <div className="flex ani_bg_p">
+      <div className="flex ani_bg_p">
         <div className="animated_bg animated_bg_1"></div>
         <div className="animated_bg animated_bg_2"></div>
-      </div> */}
+      </div>
     </>
   );
 }
